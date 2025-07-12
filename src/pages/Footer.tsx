11532847d403dcd7176492a8513919b5c985c2f3
Mohit Kumar <mohit@cognocore.com>
2025-07-12 11:22:21 +0530

    footer changed

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -65,7 +65,7 @@ const Footer = () => {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
-                <a href="" className="hover:text-indigo-300 transition-colors duration-200">
+                <a href="mailto:mohitahlawat912@gmail.com" className="hover:text-indigo-300 transition-colors duration-200">
                   mohitahlawat912@gmail.com
                 </a>
               </p>
@@ -73,7 +73,7 @@ const Footer = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-indigo-300" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
                 </svg>
-                <a href="" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-300 transition-colors duration-200">
+                <a href="https://github.com/Mohit-Kumar-3114" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-300 transition-colors duration-200">
                   github.com/Mohit-Kumar-3114
                 </a>
               </p>
@@ -82,7 +82,7 @@ const Footer = () => {
                   <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
                   <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
                 </svg>
-                <a href="" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-300 transition-colors duration-200">
+                <a href="https://www.linkedin.com/in/mohit-kumar-79866a264/" target="_blank" rel="noopener noreferrer" className="hover:text-indigo-300 transition-colors duration-200">
                   linkedin.com/in/mohit-kumar-79866a264
                 </a>
               </p>
